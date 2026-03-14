3ea2162ebb7d201b7f9566c63748e5398dc7f7ea
Raca <timotej.zavski@tutanota.com>
2026-03-14 18:17:20 +0100

    Dodan obrazec

--- views/articles/create.php
+++ views/articles/create.php
@@ -1,3 +1,19 @@
 <div class="container">
-    <h3>Objavi novico</h3>
+    <h3 class="mb-3">Objavi novico</h3>
+    <form>
+        <div class="mb-3">
+            <label class="form-label">Naslov</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Povzetek</label>
+            <textarea class="form-control" rows="3"></textarea>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Besedilo</label>
+            <textarea class="form-control" rows="8"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Objavi novico</button>
+        <label class="text-danger"></label>
+    </form>
 </div>
