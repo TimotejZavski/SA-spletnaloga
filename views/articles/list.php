9e1b408f60032f58a34c28c26dce2019e6c9fc73
Raca <timotej.zavski@tutanota.com>
2026-03-14 18:42:40 +0100

    dodan izgled za prikaz novic

---
+++ views/articles/list.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <h3>Moje novice</h3>
+    <div class="article">
+        <h4>Naslov novice</h4>
+        <p>Povzetek novice...</p>
+        <p>Objavil: uporabnik, datum</p>
+        <a href="#"><button>Preberi več</button></a>
+    </div>
+</div>
